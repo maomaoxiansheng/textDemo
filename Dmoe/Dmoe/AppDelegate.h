e116b7dcc69fa488c95c695b2a7579702452f799
MoMo <maomaoxiansheng@outlook.com>
2014-08-05 16:04:43 +0800

    1、新建项目

---
+++ Dmoe/Dmoe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Dmoe
+//
+//  Created by MoMo on 14-8-5.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dmoe/Dmoe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Dmoe
+//
+//  Created by MoMo on 14-8-5.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
